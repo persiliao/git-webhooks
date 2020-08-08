6228ba77c42366d70250b22f5983378cc1ca91bf
PersiLiao <xiangchu.liao@gmail.com>
2020-08-08 18:47:43 +0800

    update

--- src/Provider/GithubProvider.php
+++ src/Provider/GithubProvider.php
@@ -9,12 +9,15 @@ declare(strict_types=1);
 
 namespace PersiLiao\GitWebhooks\Provider;
 
+use Carbon\Carbon;
 use PersiLiao\GitWebhooks\Entity\Commit;
 use PersiLiao\GitWebhooks\Entity\Repository;
+use PersiLiao\GitWebhooks\Entity\User;
 use PersiLiao\GitWebhooks\Event\AbstractEvent;
 use PersiLiao\GitWebhooks\Event\PingEvent;
 use PersiLiao\GitWebhooks\Event\PushEvent;
 use PersiLiao\GitWebhooks\Exception\InvalidArgumentException;
+use PersiLiao\GitWebhooks\Util;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use function explode;
@@ -66,7 +69,31 @@ class GithubProvider extends AbstractProvider
 
     protected function createPushEvent(array $payload): PushEvent
     {
-        // TODO: Implement createPushEvent() method.
+        $event = new PushEvent();
+        $event->setProvider($this->provider);
+        $event->setBefore($payload['before']);
+        $event->setAfter($payload['after']);
+        $event->setRef($payload['ref']);
+
+        $user = new User();
+        $user->setName($payload['pusher']['name']);
+        if(isset($payload['pusher']['email']) && !empty($payload['pusher']['email'])){
+            $user->setEmail($payload['pusher']['email']);
+        }
+        $event->setUser($user);
+
+        $repository = new Repository();
+        $repository->setName($payload['repository']['name']);
+        $repository->setId($payload['repository']['id']);
+        $repository->setFullName($payload['repository']['full_name']);
+        $repository->setDescription($payload['repository']['description']);
+        $repository->setHomepage($payload['repository']['html_url']);
+        $repository->setUrl($payload['repository']['clone_url']);
+        $event->setRepository($repository);
+
+        $event->setCommits($this->createCommits($payload['commits']));
+        $event->setBranchName(Util::getBranchName($payload['ref']));
+        return $event;
     }
 
     protected function genreateSignature(string $secret, string $payload)
@@ -85,6 +112,19 @@ class GithubProvider extends AbstractProvider
 
     protected function createCommit(array $data): Commit
     {
-        // TODO: Implement createCommit() method.
+        $commit = new Commit();
+        $commit->setId($data['id']);
+        $commit->setMessage($data['message']);
+        $commit->setDate(new Carbon($data['timestamp']));
+        $commit->setModifieds($data['modified'] ?? []);
+        $commit->setAddeds($data['added'] ?? []);
+        $commit->setRemoveds($data['removed'] ?? []);
+        $user = new User();
+        $user->setName($data['author']['name']);
+        if(isset($data['author']['email']) && !empty($data['author']['email'])){
+            $user->setEmail($data['author']['email']);
+        }
+        $commit->setAuthor($user);
+        return $commit;
     }
 }
