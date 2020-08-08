87228153de0b2437a9fd71629c426044c89ee5bb
PersiLiao <xiangchu.liao@gmail.com>
2020-08-08 13:17:11 +0800

    update

--- src/Event.php
+++ src/Event.php
@@ -83,6 +83,7 @@ class Event
             return $provider->create();
         }
         throw new InvalidArgumentException('Git webhook not support');
+        
     }
 
     public function __call($name, $arguments)
